4a06afaa636180b2a2ef2ec7f0b3b8424aa16d7f
kisio <derrickkisio@gmail.com>
2023-06-08 20:27:39 +0300

    status

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -30,7 +30,7 @@
       </div>
 
       <div class="mb-6">
-        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 w-full hover:bg-black">
           Sign In
         </button>
       </div>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -50,7 +50,7 @@
       </div>
 
       <div class="mb-6">
-        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 w-full hover:bg-black">
           Sign Up
         </button>
       </div>
